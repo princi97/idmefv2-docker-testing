5a32d24fdd4fafdfa8103c5d2e6499cb1f1389ca
François Déchelle <fdechelle@teclib.com>
2025-12-04 12:57:19 +0100

    Fix pip invocation; wazuh works

--- wazuh+wazuh-agent+connector/containers/wazuh-idmefv2/files/entrypoint.sh
+++ wazuh+wazuh-agent+connector/containers/wazuh-idmefv2/files/entrypoint.sh
@@ -4,7 +4,6 @@ set -e
 
 # install IDMEFv2 connectors
 cd /idmefv2-connectors
-pip install --break-system-packages .
+pip install --break-system-packages --force-reinstall .
 
-# run the Wazuh IDMEFv2 connector
 python3 -m idmefv2.connectors.wazuh -c /etc/wazuh-idmefv2.conf
