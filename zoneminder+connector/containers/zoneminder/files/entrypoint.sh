3ee357ea92b0f4aa70382b89139af646867e8300
François Déchelle <fdechelle@teclib.com>
2026-01-12 19:52:14 +0100

    Add ownership fix on log file

--- zoneminder+connector/containers/zoneminder/files/entrypoint.sh
+++ zoneminder+connector/containers/zoneminder/files/entrypoint.sh
@@ -4,7 +4,12 @@ set -e
 
 # install IDMEFv2 connectors
 cd /idmefv2-connectors
-pip install --force-reinstall .
+pip install --force-reinstall --editable .
+
+# Prepare log file (path is defined in connector configuration file)
+mkdir -p /var/log/idmefv2
+touch /var/log/idmefv2/zoneminder-connector.log
+chown www-data.www-data -R /var/log/idmefv2
 
 # run the zobeminder startup script
 /etc/my_init.d/startup.sh
